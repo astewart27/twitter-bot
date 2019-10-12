c7950edd1cfe42e3b4fbb1ebe61cc8f734e30daf
Aaron Stewart <aaron.stewart.dev@gmail.com>
2019-10-12 17:56:18 -0400

    initial setup of my twitter bot

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    consumer_key:         'Yem65j4cfS0MRbUiK2d5mruSq',
+    consumer_secret:      'qcicLZEPuhd8OOWBQRouNLOPZX9g1o8MMoqiL2XnvEKxp5PNZ6',
+    access_token:         '227852160-zDqQaWKkA1JxNgR1OQEdo4wenqGJ48tUPwfEmDRt',
+    access_token_secret:  'BVlpdbFZ2ibSUwERXYIXwuDgQFm2nq8TeOcN0Q1rGqCu9',
+    timeout_ms:           60*1000
+}
---
+++ index.js
@@ -0,0 +1,33 @@
+const config = require('./config');
+const twit = require('twit');
+const T = new twit(config);
+
+// set up search params
+const params = {
+    q: '#javascript #react #reactjs #nodejs',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+}
+
+
+//  search twitter for all tweets containing #reactjs
+T.get('search/tweets', params , function(err, data, response) {
+    if(!err){
+        console.log(data);
+        for(let i = 0; i < data.statuses.length; i++) {
+            let id = {id: data.statuses[i].id_str};
+            T.post('favorites/create', id, function(err, res) {
+                if(!err) {
+                    let username = res.user.screen_name;
+                    let tweetId = res.id_str;
+                    console.log(`Favorited: https://twitter.com/${username}/status/${tweetId}`);
+                } else {
+                    console.log(err[0].message);
+                }
+            });
+        }
+    } else {
+        console.log(err);
+    }
+});
